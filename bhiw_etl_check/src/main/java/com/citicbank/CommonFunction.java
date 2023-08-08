16375c4b64615d549366467667959b5f80d8699b
lkk <kscrpt@163.com>
2023-08-09 02:08:22 +0800

    代码检查

--- bhiw_etl_check/src/main/java/com/citicbank/CommonFunction.java
+++ bhiw_etl_check/src/main/java/com/citicbank/CommonFunction.java
@@ -5,12 +5,13 @@ import java.io.File;
 public class CommonFunction {
 
 
-
+    //检查文件是否存在
     public static  boolean isFileExists(String csvFile) {
         File file = new File(csvFile);
         return file.exists();
     }
 
+    //统计目录下的文件个数
     public static int countFilesInDirectory(String directoryPath) {
 
         File directory = new File(directoryPath);
@@ -28,5 +29,10 @@ public class CommonFunction {
         }
         return fileCount;
     }
+
+    //检查对应文件是否是UTF-8格式
+
+    //检查代码文件的换行符是否是UNIX(LF)而不是WINDOWS(CR LF)
+
 
 }
